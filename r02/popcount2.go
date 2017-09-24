2d6cdb6f41f61791053a9f2f9dae08dd227b6680
Patryk <patryk.targosinski@gmail.com>
2017-09-24 21:03:34 +0200

    exercise 2.3; need to compare with popcount.go timewise

---
+++ r02/popcount2.go
@@ -0,0 +1,18 @@
+package popcount
+
+//pc[i] jest liczebnoscia populacji i.
+var pc [256]byte
+
+func init() {
+    for i := range pc {
+        pc[i] = pc[i/2] + byte(i&1)
+    }
+}
+
+//PopCountzwraca liczebnosc populacji (liczbe ustawionych bitow) dla x.
+func PopCount(x uint64) int {
+    var result byte
+    for i := 0; i < 8; i++ {
+        result += pc[byte(x>>(i*8))]
+    return int(result)
+}
