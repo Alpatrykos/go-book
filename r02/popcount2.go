eca067cab648babd4a9177ce1b28349bc65d3c8e
Patryk <patryk.targosinski@gmail.com>
2017-09-26 21:25:51 +0200

    exercise 2.5; need to check the perfo now

--- r02/popcount2.go
+++ r02/popcount2.go
@@ -4,16 +4,16 @@ package popcount
 var pc [256]byte
 
 func init() {
-    for i := range pc {
-        pc[i] = pc[i/2] + byte(i&1)
-    }
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
 }
 
 //PopCountzwraca liczebnosc populacji (liczbe ustawionych bitow) dla x.
 func PopCount(x uint64) int {
-    var result byte
-    for i := 0; i < 8; i++ {
-        result += pc[byte(x>>uint(i*8))]
-    }
-    return int(result)
+	var result byte
+	for i := 0; i < 8; i++ {
+		result += pc[byte(x>>uint(i*8))]
+	}
+	return int(result)
 }
---
+++ r02/popcount_clearLSB.go
@@ -0,0 +1,14 @@
+package popcount_exercise
+
+//pc[i] jest liczebnoscia populacji i.
+var pc [256]byte
+
+//PopCountzwraca liczebnosc populacji (liczbe ustawionych bitow) dla x.
+func PopCount(x uint64) int {
+	var result int
+	for x < 0 {
+		result++
+		x &= (x - 1)
+	}
+	return result
+}
