bc4b2e948d1444d182aa47f69166401e05ece1bf
Patryk <patryk.targosinski@gmail.com>
2017-10-04 22:05:27 +0200

    exercise 3.5 color using color.RGBA

--- r03/mandelbrot/main.go
+++ r03/mandelbrot/main.go
@@ -29,13 +29,30 @@ func main() {
 
 func mandelbrot(z complex128) color.Color {
     const iterations = 200
-    const contrast = 15
+    const contrast = 10
 
     var v complex128
     for n := uint8(0); n < iterations; n++ {
         v = v*v + z
         if cmplx.Abs(v) > 2 {
-            return color.Gray{255 - contrast*n}
+            r, b, g := uint8(0), uint8(0), uint8(0)
+            a := uint8(255)
+            sum := 3*255
+            sum -= int(n)*contrast
+            switch sum/255 {
+            case 3:
+                r,b,g = 255, 255, 255
+            case 2:
+                r,b = 255, 255
+                g = uint8(sum % 255)
+            case 1:
+                r = 255
+                g = 0
+                b = uint8(sum % 255)
+            default:
+                r,b,g = 0, 0, 0
+            }
+            return color.RGBA{r, b, g, a}
         }
     }
     return color.Black
