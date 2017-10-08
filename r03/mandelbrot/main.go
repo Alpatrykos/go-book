54ea882afc175a61c27f9c0162eb00d1ae5825a2
Patryk <patryk.targosinski@gmail.com>
2017-10-08 20:22:37 +0200

    another supersampling implemented for ex 3.6; does not decrease resolution that much

--- r03/mandelbrot/main.go
+++ r03/mandelbrot/main.go
@@ -25,7 +25,7 @@ func main() {
             img.Set(px, py, mandelbrot(z))
         }
     }
-    rimg := supersample(img)
+    rimg := supersample2(img)
     png.Encode(os.Stdout, rimg) //UWAGA: ignorowanie bledow
 }
 
@@ -48,6 +48,8 @@ func mandelbrot(z complex128) color.Color {
     return color.Black
 }
 
+// does not work as intended; the image actually becomes more aliased in a result of supersampling
+// method implemented in the fuction.
 func supersample(img image.Image) image.Image {
     bounds := img.Bounds()
     ssimg := image.NewRGBA(image.Rect(0, 0, bounds.Max.X/2, bounds.Max.Y/2))
@@ -65,4 +67,23 @@ func supersample(img image.Image) image.Image {
        }
    }
    return ssimg
+}
+
+func supersample2(img image.Image) image.Image {
+    bounds := img.Bounds()
+    ssimg := image.NewRGBA(image.Rect(0, 0, bounds.Max.X-1, bounds.Max.Y-1))
+    for py := 0; py < ssimg.Bounds().Max.Y; py++ {
+        for px := 0; px < ssimg.Bounds().Max.X; px++ {
+            r1, g1, b1, _ := img.At(px, py).RGBA()
+            r2, g2, b2, _ := img.At(px+1, py).RGBA()
+            r3, g3, b3, _ := img.At(px, py+1).RGBA()
+            r4, g4, b4, _ := img.At(px+1, py+1).RGBA()
+            avg_r := float64(r1 + r2 + r3 + r4)
+            avg_g := float64(g1 + g2 + g3 +g4)
+            avg_b := float64(b1 + b2 + b3 + b4)
+            c := color.RGBA{uint8(avg_r/4), uint8(avg_g/4), uint8(avg_b/4), 255}
+            ssimg.Set(px, py, c)
+        }
+    }
+    return ssimg
 }
