b93a8ed5729280420dfd84b066a7eff4d94aa246
Patryk <patryk.targosinski@gmail.com>
2017-10-09 17:42:20 +0200

    exercise 3.10 fixed

--- r03/comma/main.go
+++ r03/comma/main.go
@@ -6,47 +6,24 @@ import (
     "os"
 )
 
-const dash = '-'
-
 var buf bytes.Buffer
-var fract string
 //comma dodaje znak przecinka co 3 cyfry w lancuchu liczby rzeczywistej
-func comma(num1 string) string {
-    num := num1
-    if num[0] == dash {
-        buf.WriteByte('-')
-        num = num[1:]
+func comma(num string) string {
+    n := len(num)
+    var s string
+    if n <= 3 {
+        return num
     }
-    for i := 0; i < len(num); i++ {
-        if num[i] == '.' {
-            fract = num[i:]
-            num = num[:i]
-        }
+    buf.WriteString(num)
+    for buf.Len() > 3 {
+        s = string(',') + buf.String()[buf.Len()-3:] + s
+        buf.Truncate(buf.Len() - 3)
     }
-    var nnum bytes.Buffer
-    if len(num) <= 3 {
-        nnum.WriteString(num)
-    }else {
-        var temp string
-        nnum.WriteString(num[len(num)-3:])
-        num = num[:len(num)-3]
-        for len(num) > 3 {
-            temp = nnum.String()
-            nnum.Reset()
-            fmt.Fprintf(&nnum, "%s,%s", num[len(num)-3:], temp)
-            num = num[:len(num)-3]
-            if len(num) < 3 && len(num) > 0 {
-                temp = nnum.String()
-                nnum.Reset()
-                fmt.Fprintf(&nnum, "%s,%s", num, temp)
-            }
-        }
+    if buf.Len() > 0 {
+        s = buf.String() + s
     }
-    buf.WriteString(nnum.String())
-    buf.WriteString(fract)
-    return buf.String()
+    return s
 }
-
 func main() {
     arg := os.Args[1]
     fmt.Printf("%s\n", comma(arg))
