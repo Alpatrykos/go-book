56057004f6da0430b22244420852b3804e94a235
Patryk <patryk.targosinski@gmail.com>
2017-10-11 21:22:43 +0200

    exercise 4.6

---
+++ r04/rmSpace/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "fmt"
+    "unicode"
+    "unicode/utf8"
+)
+
+const str = "  ada      aaa"
+
+func main() {
+    fmt.Println(str)
+    fmt.Println([]byte(str))
+    s := rmSpace([]byte(str))
+    fmt.Println(s)
+}
+
+func rmSpace(str []byte) []byte {
+    s := string(str)
+    size := 0
+    prev_space := false
+    for _, r := range s {
+        if unicode.IsSpace(r) {
+            if prev_space {
+                continue
+            } else {
+                prev_space = true
+            }
+        } else {
+            prev_space = false
+        }
+        n := utf8.RuneLen(r)
+        copy(str[size:size+n], []byte(string(r)))
+        size += n
+    }
+    return str[:size]
+}
