02d280f51df9e678c7106d96499c4474de4676c3
Patryk <patryk.targosinski@gmail.com>
2017-10-15 20:54:37 +0200

    exercise 4.10 done

--- r04/issues/issues.go
+++ r04/issues/issues.go
@@ -5,6 +5,7 @@ import (
     "log"
     "os"
     "../github"
+    "time"
 )
 
 func main() {
@@ -13,8 +14,30 @@ func main() {
         log.Fatal(err)
     }
     fmt.Printf("%d tematow:\n", result.TotalCount)
+    now := time.Now()
+    monthAgo := now.AddDate(0, -1, 0)
+    yearAgo := now.AddDate(-1, 0, 0)
+    fmt.Println(" < Month ago:")
     for _, item := range result.Items {
-        fmt.Printf("#%-5d %9.9s %.55s\n",
-        item.Number, item.User.Login, item.Title)
+        if item.CreatedAt.After(monthAgo) {
+            fmt.Printf("#%-5d %9.9s %60.60s %15s\n",
+            item.Number, item.User.Login, item.Title, item.CreatedAt)
+        }
     }
+    fmt.Println("\n < Year ago:")
+    for _, item := range result.Items {
+        if item.CreatedAt.After(yearAgo) {
+            fmt.Printf("#%-5d %9.9s %60.60s %15s\n",
+            item.Number, item.User.Login, item.Title, item.CreatedAt)
+        }
+    }
+ 
+    fmt.Println("\n > Year ago:")
+    for _, item := range result.Items {
+        if item.CreatedAt.Before(yearAgo) {
+            fmt.Printf("#%-5d %9.9s %60.60s %15s\n",
+            item.Number, item.User.Login, item.Title, item.CreatedAt)
+        }
+    }
+
 }
