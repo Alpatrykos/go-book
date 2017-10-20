e85853f665b6926aaadd63967d7e7938350aad1e
Patryk <patryk.targosinski@gmail.com>
2017-10-20 21:42:31 +0200

    add few details in comments

--- r04/github-issues/github-issues.go
+++ r04/github-issues/github-issues.go
@@ -1,5 +1,6 @@
 // Exercise 4.11 
-// Create a tool that allows the user to create, read, update and delete (basically CRUD) application 
+// Create a tool that allows the user to create, read, update and delete issues at github 
+// (basically CRUD) application 
 // that uses github api to operate. Evoke Vim editor when there is a neccessity to write a considerable
 // amount of text.
 package main
@@ -10,7 +11,7 @@ import (
 )
 
 // get github authentication token (alternatively a config file can be used)
-// Note that the OAuth access token is 
+// Note that the OAuth access token is a environment variable
 const authToken = os.Getenv("GITHUB_OAUTH_TOKEN")
 
 func main() {
